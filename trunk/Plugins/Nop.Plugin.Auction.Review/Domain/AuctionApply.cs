c22f305e00047b7f243b21a97696c86451ea9f8f
xcoolray <285025600@qq.com>
2014-08-25 17:05:55 +0800

    test

---
+++ trunk/Plugins/Nop.Plugin.Auction.Review/Domain/AuctionApply.cs
@@ -0,0 +1,14 @@
+﻿using Nop.Core;
+namespace Nop.Plugin.Auction.Review.Domain
+{
+    public partial class AuctionApply : BaseEntity
+    {
+        public int ProductId { get; set; }
+
+        public int CustomerId { get; set; }
+
+        public string Description { get; set; }
+
+
+    }
+}
